47e3ad129a20ba039a5a6ab931977ec20c298ef0
Noreen Jenn Tajoda <noreenjenn@Noreens-MacBook-Air.local>
2022-08-26 14:54:44 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.get("/")
+@app.getç("/home", response_class=HTMLResponse)
+async def home(request:Request):
+    return templates.TemplateResponse("home.html", {"request": request, "posts": posts})
+
